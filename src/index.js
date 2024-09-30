798c125aa3d437951877ffe6136e80dd097a5822
Tezromania <taylorstapleton@gmail.com>
2024-09-30 22:30:44 +0000

    fix url

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as PIXI from "pixi.js";
 const app = new PIXI.Application();
 
 async function test(container) {
-  let url = new URL("fish2.png", import.meta.url);
+  let url = new URL("../fish2.png", import.meta.url);
   await PIXI.Assets.load(url);
   let sprite = PIXI.Sprite.from(url);
   container.addChild(sprite);
