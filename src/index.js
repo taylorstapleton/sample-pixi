666b2adf1c73001eae5cb099bd122a68ef06fedc
Tezromania <taylorstapleton@gmail.com>
2024-09-30 17:36:12 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import * as PIXI from "pixi.js";
+const app = new PIXI.Application();
+
+async function test(container) {
+  await PIXI.Assets.load("fish2.png");
+  let sprite = PIXI.Sprite.from("fish2.png");
+  container.addChild(sprite);
+}
+
+(async () => {
+  await app.init({ background: "#2d3338", resizeTo: window });
+  document.body.appendChild(app.canvas);
+  const container = new PIXI.Container();
+  app.stage.addChild(container);
+  await test(container);
+  console.log("it works");
+})();
